9562fe1dfdc14fd878c6d423f111a1c061792f04
Sergen Bakır <cuseg09@gmail.com>
2023-03-01 21:20:44 +0300

    RequestModels Added

---
+++ src/Common/BlazorSozluk.Common/Models/RequestModels/CreateEntryCommentVoteCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Common.Models.RequestModels
+{
+    public class CreateEntryCommentVoteCommand : IRequest<bool>
+    {
+        public Guid EntryCommentId { get; set; }
+
+        public VoteType VoteType { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public CreateEntryCommentVoteCommand()
+        {
+
+        }
+
+        public CreateEntryCommentVoteCommand(Guid entryCommentId, VoteType voteType, Guid createdBy)
+        {
+            EntryCommentId = entryCommentId;
+            VoteType = voteType;
+            CreatedBy = createdBy;
+        }
+    }
+
+}
