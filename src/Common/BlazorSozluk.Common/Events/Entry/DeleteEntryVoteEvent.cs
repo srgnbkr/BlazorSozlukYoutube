8c1fcf3352176d5276820c0d3f1dbb118cfcd58b
Sergen Bakır <cuseg09@gmail.com>
2023-03-01 22:54:36 +0300

    Events added

---
+++ src/Common/BlazorSozluk.Common/Events/Entry/DeleteEntryVoteEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Common.Events.Entry
+{
+    public class DeleteEntryVoteEvent
+    {
+        public Guid EntryId { get; set; }
+
+        public Guid CreatedBy { get; set; }
+    }
+
+}
